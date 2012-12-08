3042b0c2587db26672ea047451c959295a46a3ae
Ken Snyder <kendsnyder@gmail.com>
2012-12-08 15:10:28 -0700

    c 3

--- Game.js
+++ Game.js
@@ -18,6 +18,12 @@ Game.prototype = {
 		if (!this.grid[x]) 
 			this.grid[x] = [];
 	},
+	shouldLive: function(cellx, celly) {
+		for (var x = -1; x <= 1; x++) {
+			
+		}
+		return false;
+	},
 	tick: function() {
 		var x, y;
 		for (x = 0; x < 10; x++) {
