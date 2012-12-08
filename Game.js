c6cec48660900e9cea398280fc7df40d9edf618c
Ken Snyder <kendsnyder@gmail.com>
2012-12-08 14:54:01 -0700

    c 3

--- Game.js
+++ Game.js
@@ -4,13 +4,19 @@ function Game() {
 
 Game.prototype = {
 	addPoint: function(x,y) {
+		this.goingToUsePoint(x,y);
 		this.grid[x][y] = 1;
 	},
 	isAlive: function(x,y) {
 		return this.grid[x] && !!this.grid[x][y];
 	},
 	killAt: function(x, y) {
+		this.goingToUsePoint(x, y)
 		this.grid[x][y] = 0;
+	},
+	goingToUsePoint: function(x, y) {
+		if (!this.grid[x]) 
+			this.grid[x] = [];
 	}
 };
 
